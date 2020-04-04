9c38036f67514a59acde0e82b431be78dc8544b9
Natalie Smyth <nataliesmyth11@gmail.com>
2020-04-04 07:29:17 -0400

    created game logic

--- memory-game/js/main.js
+++ memory-game/js/main.js
@@ -1,12 +1,24 @@
-console.log("Up and running!");
 
-let cardOne = "queen";
 
-let cardTwo = "king";
+let cards = ["queen", "queen", "king", "king"];
+let cardsInPlay = [];
+let cardOne = cards[3];
 
-let cardThree = "queen"
+cardsInPlay.push(cardOne);
+console.log("User flipped queen");
 
-let cardFour = "king"
+let cardTwo = cards[2];
+cardsInPlay.push(cardTwo);
+console.log("User flipped king")
 
-console.log("User flipped " + cardOne);
-console.log("User flipped" + cardTwo);
+
+
+if (cardsInPlay.length === 2) {
+
+	 
+	 if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
